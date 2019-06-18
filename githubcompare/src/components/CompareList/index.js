6da5f58ca97cc57fabd23f6e15c3eeff6fac7a43
Rodrigo Dias <diasbass@hotmail.com>
2019-06-18 16:22:30 -0300

    Lista de Repositórios

---
+++ githubcompare/src/components/CompareList/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const CompareList = () => (
+  <div>
+    <div>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="" />
+        <strong>React</strong>
+        <small>Facebook</small>
+      </header>
+      <ul>
+        <li>
+          95,019 <small>stars</small>
+        </li>
+        <li>
+          95,019 <small>forks</small>
+        </li>
+        <li>
+          95,019 <small>issues</small>
+        </li>
+        <li>
+          95,019 <small>last commit</small>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default CompareList;
